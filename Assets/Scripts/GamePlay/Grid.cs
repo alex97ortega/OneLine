35622c403ba8b58995fba3361b56acfffd2f90c6
alex97ortega <alejor01@ucm.es>
2020-02-08 12:14:56 +0100

    Vuelta a versión de Unity 2018.3.0f2

--- Assets/Scripts/GamePlay/Grid.cs
+++ Assets/Scripts/GamePlay/Grid.cs
@@ -164,6 +164,9 @@ public class Grid : MonoBehaviour {
         {
             if (b && b.Inside(x,y))
             {
+                SoundManager sm = FindObjectOfType<SoundManager>();
+                if (sm)
+                    sm.PlayTapSound();
                 b.Tap();
                 return;
             }
